9ed17c2f6e2db76c8c3b7ec27bb0fb03b019de42
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-10-19 21:05:24 +0800

    Added animation to nav items

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -6,8 +6,9 @@ const NavbarItems = ({ showContact }) => {
         <>
             {/* Menu items for the navigation */}
             <div className="text-sm md:flex-grow md:text-right">
-                <Link to="/" className="block mt-4 mr-4 md:inline-block font-medium md:mt-0 text-gray-300 focus:outline-none">Home</Link>
-                <Link to="/projects" className="block mt-4 mr-4 md:inline-block font-medium md:mt-0 text-gray-300 focus:outline-none">Projects</Link>
+                <Link to="/" className="transition duration-500 ease-in-out md:transform md:hover:scale-105 block mt-4 mr-4 md:inline-block font-medium md:mt-0 text-gray-300 focus:outline-none">Home</Link>
+                <Link to="/projects" className="transition duration-500 ease-in-out md:transform md:hover:scale-105 block mt-4 mr-4 md:inline-block font-medium md:mt-0 text-gray-300 focus:outline-none">Software Projects</Link>
+                <Link to="/projects" className="transition duration-500 ease-in-out md:transform md:hover:scale-105 block mt-4 mr-4 md:inline-block font-medium md:mt-0 text-gray-300 focus:outline-none">Web Projects</Link>
                 <button onClick={() => showContact()} className="transition duration-500 ease-in-out font-medium text-gray-300 mt-4 md:bg-primary md:transform md:hover:scale-105 md:text-white md:mt-0 md:py-2 md:px-4 md:rounded-full focus:outline-none">Have a chat</button>
             </div>
         </>
