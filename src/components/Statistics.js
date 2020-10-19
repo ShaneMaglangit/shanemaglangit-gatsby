154d0808355d72352c4960a228b1b714a7007cc6
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-10-19 23:24:58 +0800

    Updated statistics

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,10 +1,20 @@
 import React from "react"
 
 const Statistics = () => {
+    let startDate = new Date(2019, 9, 8, 0, 0, 0, 0);
+    let currentDate = new Date();
+
     let clientCount = 20;
-    let websiteCount = 20;
-    let softwareCount = 40;
-    let daysAsDeveloper = 50;
+    let websiteCount = 11;
+    let softwareCount = 31;
+    let daysAsDeveloper = getDateDiff(currentDate, startDate);
+
+    function getDateDiff(x, y) {
+        const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+        const xUTC = Date.UTC(x.getFullYear(), x.getMonth(), x.getDate());
+        const yUTC = Date.UTC(y.getFullYear(), y.getMonth(), y.getDate());
+        return Math.floor((xUTC - yUTC) / _MS_PER_DAY);
+    }
 
     return (
         <>
@@ -14,7 +24,7 @@ const Statistics = () => {
                         <div className="bg-primary w-1 border border-primary"></div>
                         <div className="flex items-center flex-grow py-4 px-10">
                             <h2 className="text-5xl text-right text-primary font-bold">{daysAsDeveloper}</h2>
-                            <h3 className="text-lg pl-4 text-white">DAYS AS A DEVELOPER</h3>
+                            <h3 className="text-lg pl-4 text-white">DAYS SINCE MY FIRST PROJECT</h3>
                         </div>
                     </div>
                 </div>
@@ -23,7 +33,7 @@ const Statistics = () => {
                         <div className="bg-primary w-1 border border-primary"></div>
                         <div className="flex items-center flex-grow py-4 px-10">
                             <h2 className="text-5xl text-right text-primary font-bold">{clientCount}</h2>
-                            <h3 className="text-lg pl-4 text-white">HAPPY CLIENTS</h3>
+                            <h3 className="text-lg pl-4 text-white">SATISFIED AND HAPPY CLIENTS</h3>
                         </div>
                     </div>
                 </div>
@@ -32,7 +42,7 @@ const Statistics = () => {
                         <div className="bg-primary w-1 border border-primary"></div>
                         <div className="flex items-center flex-grow py-4 px-10">
                             <h2 className="text-5xl text-right text-primary font-bold">{softwareCount}</h2>
-                            <h3 className="text-lg pl-4 text-white">SOFTWARE DEVELOPED</h3>
+                            <h3 className="text-lg pl-4 text-white">SOFTWARE PROJECTS COMPLETED </h3>
                         </div>
                     </div>
                 </div>
@@ -41,7 +51,7 @@ const Statistics = () => {
                         <div className="bg-primary w-1 border border-primary"></div>
                         <div className="flex items-center flex-grow py-4 px-10">
                             <h2 className="text-5xl text-right text-primary font-bold">{websiteCount}</h2>
-                            <h3 className="text-lg pl-4 text-white">WEBSITES BUILT</h3>
+                            <h3 className="text-lg pl-4 text-white">WEBSITES BUILT AND DEPLOYED</h3>
                         </div>
                     </div>
                 </div>
